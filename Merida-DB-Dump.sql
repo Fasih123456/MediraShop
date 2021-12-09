740b0c2e4ace33e51b7cbb766bc40178db307937
ZhenYu Lei <zh772515@dal.ca>
2021-12-08 21:14:45 -0400

    change db

--- Merida-DB-Dump.sql
+++ Merida-DB-Dump.sql
@@ -1,7 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 4.7.2
 -- https://www.phpmyadmin.net/
---
 -- Host: localhost:3306
 -- Generation Time: Nov 30, 2021 at 2:00pm
 -- Server version: 5.6.35
@@ -33,12 +32,13 @@ CREATE TABLE `m_login` (
 --
 -- Dumping data for table `m_login`
 --
---Default password is Post@123
+-- Default password is Post@123
 INSERT INTO `m_login` (`m_id`, `m_email`, `m_password`) VALUES
-(1, 'user@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC'),
-(2, 'user2@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC'),
-(3, 'user3@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC'),
-(4, 'user4@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC');
+(1, 'Abdullah@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC'),
+(2, 'Nathaniel@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC'),
+(3, 'Fasih@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC'),
+(4, 'ZhenYu@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC'),
+(5, 'Dorian@mail.com', '$2y$10$9DRkxzSH1O1KvxHIoykjyOAshB0xhbl8tcmOKxCnpSrTqIlZs6WeC');
 
 -- --------------------------------------------------------
 
@@ -62,10 +62,11 @@ CREATE TABLE `m_account` (
 -- Dumping data for table `m_account`
 --
 INSERT INTO `m_account` (`m_account_id`, `m_account_fname`, `m_account_lname`, `m_login_id`, `m_account_location`, `m_seller`) VALUES
-(1, 'first', 'user', '1', 'Halifax', 'TRUE'),
-(2, 'second', 'user', '2', 'Halifax', 'FALSE'),
-(3, 'third', 'user', '3', 'Halifax', 'FALSE'),
-(4, 'fourth', 'user', '4', 'Dartmouth', 'TRUE');
+(1, 'Abdullah', 'AI Mukaddim', '1', 'Halifax', 1),
+(2, 'Nathaniel', 'Wilson', '2', 'Halifax', 1),
+(3, 'Fasih', 'UI Islam', '3', 'Halifax', 1),
+(4, 'ZhenYu', 'Lei', '4', 'Dartmouth', 0),
+(5, 'Dorian', 'Germain Zambo Zambo', '5', 'Dartmouth', 0);
 
 -- --------------------------------------------------------
 --
@@ -94,8 +95,8 @@ INSERT INTO `m_goods` (`m_goods_id`, `m_account_id`, `m_goods_type`, `m_goods_na
 (3, '2', 'Grocery', 'Orange', 'Florida navel oranges 3lb', '5.99', 'images/Orange.jpg'),
 (4, '2', 'Electronics', 'Phone', 'Brand new smartphone, warranty up to 3 years', '500.00', 'images/Phone.jpg'),
 (5, '2', 'Clothing', 'scarf', 'Red, wool, men and women', '19.99', 'images/Scarf.jpg'),
-(6, '2', 'Clothing', 'T-shirt', 'Blue cotton shirt from nameBrand company', '39.99', 'images/T-Shirt.jpg'),
-(7, '2', 'Gifts', 'Gift Card', 'E-gift card, $25.00', '25.00', 'images/Gift Card.jpg');
+(6, '3', 'Clothing', 'T-shirt', 'Blue cotton shirt from nameBrand company', '39.99', 'images/T-Shirt.jpg'),
+(7, '3', 'Gifts', 'Gift Card', 'E-gift card, $25.00', '25.00', 'images/Gift Card.jpg');
 
 -- --------------------------------------------------------
 --
@@ -124,9 +125,11 @@ CREATE TABLE `m_card` (
 -- Dumping data for table `m_account`
 --
 INSERT INTO `m_card` (`m_card_id`, `m_account_id`, `m_card_fname`,`m_card_lname`, `m_card_address`,`m_card_country`, `m_card_city`, `m_card_province`, `m_card_postal`, `m_card_number`,`m_card_expire`, `m_card_cvv`) VALUES
-(1, 1, 'first', 'user', '123 Street', 'Canada','Halifax','Nova Scotia', 'A0A-0A0','0000-1111-2222-3333', '2024-01-01', '111'),
-(2, 2, 'second', 'user', '123 Street', 'Canada','Halifax','Nova Scotia', 'A0A-0A0','0000-1111-2222-3333', '2024-01-01', '111'),
-(3, 3, 'third', 'user', '123 Street', 'Canada','Halifax','Nova Scotia', 'A0A-0A0', '0000-1111-2222-3333', '2024-01-01', '111'),
-(4, 4, 'fourth', 'user', '123 Street', 'Canada','Dartmouth','Nova Scotia', 'A0A-0A0','0000-1111-2222-3333', '2024-01-01', '111');
+(1, 3, 'Fasih', 'UI Islam', '123 Street', 'Canada','Halifax','Nova Scotia', 'A0A-0A0','0000-1111-2222-3333', '2024-01-01', '111'),
+(2, 3, 'Fasih', 'UI Islam', '123 Street', 'Canada','Halifax','Nova Scotia', 'A0A-0A0','0000-1111-2222-3333', '2024-01-01', '111'),
+(3, 4, 'ZhenYu', 'Lei', '123 Street', 'Canada','Halifax','Nova Scotia', 'A0A-0A0', '0000-1111-2222-3333', '2024-01-01', '111'),
+(4, 4, 'ZhenYu', 'Lei', '123 Street', 'Canada','Dartmouth','Nova Scotia', 'A0A-0A0','0000-1111-2222-3333', '2024-01-01', '111'),
+(5, 5, 'Dorian', 'Germain Zambo Zambo', '123 Street', 'Canada','Dartmouth','Nova Scotia', 'A0A-0A0','0000-1111-2222-3333', '2024-01-01', '111');
+
 
 
